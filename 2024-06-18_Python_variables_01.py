a58beb3e351366502a7c8fc9cfc9e18c29445807
maksym-vk <maksym.volodymyrovych@gmail.com>
2024-06-21 18:43:00 +0300

    2024-06-21_18-42 updates

--- 2024-06-18_Python_variables_01.py
+++ 2024-06-18_Python_variables_01.py
@@ -1,12 +1,4 @@
-# Take a look at the list of words that play a very special role in every Python program.
-#
-# ['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del',
-# 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is',
-# 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
-#
-# They are called keywords or (more precisely) reserved keywords.
-# They are reserved because you mustn't use them as names: neither for your variables,
-# nor functions, nor any other named entities you want to create.
+
 
 var = "3.8.5"
 print("Python version: " + var) # the same as
--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 
 
 print("Hello Python!")
+
+
+# If you'd like to quickly comment or uncomment multiple lines of code, select the line(s)
+# you wish to modify and use the following keyboard shortcut:
+# CTRL + / (Windows) or
+# CMD + / (Mac OS).
+# It's a very useful trick, isn't it? Now experiment with the code in the editor.
+
+
+# Take a look at the list of words that play a very special role in every Python program.
+#
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del',
+# 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is',
+# 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+#
+# They are called keywords or (more precisely) reserved keywords.
+# They are reserved because you mustn't use them as names: neither for your variables,
+# nor functions, nor any other named entities you want to create.
